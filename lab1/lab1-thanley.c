d6b3f4c7bdef94d4edfc27455b4733eccc41311c
Tommy Hanley <thanley@oswego.edu>
2018-09-25 22:22:15 -0400

    updated lab 1

--- lab1/lab1-thanley.c
+++ lab1/lab1-thanley.c
@@ -10,59 +10,120 @@
  * lanac a, nalp a, nam a
  */
 
-void palin() {
+void palin(char *forwards) {
+	char *backwards = malloc(sizeof(forwards));
+	int f = strlen(forwards) - 1;
+	int b = 0;
+	while(b < strlen(forwards)) {
+		backwards[b] = forwards[f];
+		f--;
+		b++;
+	}
 
+	for(int j = 0; j < sizeof(forwards); j++){
+	  if(forwards[j] == ' ' || forwards[j] == ',' || forwards[j] == '.' || forwards[j] == ';'
+	     || forwards[j] == ':' || forwards[j] == '!' || forwards[j] == '?') {
+	    for(int p = j; p <= sizeof(forwards); p++)
+	       forwards[p] = forwards[p+1];
+	  }
+        }
+	
+	char *backnospace = malloc(sizeof(backwards));
+	strcpy(backnospace, backwards);
+
+	for(int u = 0; u < sizeof(backnospace); u++){
+	  if(backnospace[u] == ' ' || backnospace[u] == ',' || backnospace[u] == '.' || backnospace[u] == ';'
+	     || backnospace[u] == ':' || backnospace[u] == '!' || backnospace[u] == '?') {
+	    for(int p = u; p <= sizeof(backnospace); p++)
+	       backnospace[p] = backnospace[p+1];
+	  }
+        }
+	
+	printf("%s for, %s back\n", forwards,backnospace);
+	//Since we have the arguments in forward and reverse, compare the two
+	if(strcmp(forwards, backnospace) == 0)
+		printf("%s <- palindrome \n", backwards);
+	else
+		printf("%s <- no palindrome \n", backwards);
+	free(backwards);
+	free(backnospace);
 }
 
 //Takes in a string, and a number for the switch case on what to change the chars to
-void cascnvt(char **c, int s) {
+char* cascnvt(char *c, int s) {
 	switch(s) {
 		case 0:
 			//reverse the cases that are given
 			for(int i = 0; i < strlen(c); i++) {
-				if(islower(c[i]) == 0) {
-					toupper(c[i]);
-					printf("%c\n", c[i]);
-				}
-				else {
-					tolower(c[i]);
-					printf("you fucked up\n");
-				}
+				//Change a lower case letter to upper case
+				if(islower(c[i]) != 0) 
+					c[i] = toupper(c[i]);
+				else 
+					c[i] = tolower(c[i]);
 			}
 			break;
 		case 1:
-			//convert all the chars to lower case
-		printf("this didnt work\n");
+			//This case will convert all the chars to lower case
+			//Iterate through the char pointer
+			for(int i = 0; i < strlen(c); i++) {
+				//Change the char at i to lower case
+				c[i] = tolower(c[i]);
+			}
 			break;
 		case 2:
 			//convert all the chars to upper case
-			printf("this didnt work\n");
+			for(int i = 0; i < strlen(c); i++) {
+				//Change the char at i to upper case
+				c[i] = toupper(c[i]);
+			}
 			break;
 		default:
 			printf("You fucked up\n");
 			break;
 	}
-	printf("This: %s\n", *c);
+
+	return c;
 }
 
 int main(void) {
-	char buffer[102];
-	printf("%s", "Enter a command: \n");
+  printf("%s\n", "Enter cascnvt, palin, or exit");
+	printf("%s", "thanley>$ ");
 	char cmd[67];
-	scanf("%s", cmd);
+	//scanf("%s", cmd);
+	fgets(cmd, sizeof(cmd), stdin);
 	char *token;
-	char breaker[2] = " ";
-	token = strtok(cmd, breaker);
+	char *decisions;
+	token = strtok(cmd, "(");
+	decisions = strtok(NULL, ")");
 	while(strcmp(token, "exit") != 0) {
-		//printf("%s", buffer);
-		if(strcmp(token, "palin") == 0) {
-			printf("this worked\n");
-			char *k = "ab";
-			int j = 0;
-			cascnvt(&k, 0);
+		//If the user enters cascnvt
+		if(strcmp(token, "cascnvt") == 0) {
+			//tokenize the string to be adjusted
+			char *tokenTwo;
+			tokenTwo = strtok(decisions, ",");
+			char *tokenThree;
+			tokenThree = strtok(NULL, ")");
+			if(strcmp(tokenThree, "0") == 0)
+				tokenTwo = cascnvt(tokenTwo, 0);
+			else if(strcmp(tokenThree, "1") == 0)
+				tokenTwo = cascnvt(tokenTwo, 1);
+			else if(strcmp(tokenThree, "2") == 0)
+				tokenTwo = cascnvt(tokenTwo, 2);
+			else
+				printf("Incorrect");
+			printf("%s\n", tokenTwo);
 		}
-		printf("%s", "Enter a command: \n");
-		scanf("%s", cmd);
-		token = strtok(cmd, breaker);
+
+		//If the user enters palin
+		if(strcmp(token, "palin") == 0) {
+			int i = 0;
+			int j = 6;
+			palin(decisions);
+		}
+		printf("%s\n", "Enter cascnvt, palin, or exit ");
+		printf("%s", "thanley>$");
+		fgets(cmd, sizeof(cmd), stdin);
+		token = strtok(cmd, "(");
+		decisions = strtok(NULL, ")");
 	}
 }
